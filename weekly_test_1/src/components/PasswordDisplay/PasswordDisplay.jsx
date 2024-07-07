ee6f7bd338e59e42c2b6baf0528ae8cfc7fff2e5
Varun Arora <varunarora137@gmail.com>
2024-07-07 15:26:16 +0530

    modified weekly_test_1

--- weekly_test_1/src/components/PasswordDisplay/PasswordDisplay.jsx
+++ weekly_test_1/src/components/PasswordDisplay/PasswordDisplay.jsx
@@ -8,7 +8,12 @@ function PasswordDisplay(props) {
       <h1>Password Generator</h1>
       <div className="inputAndButton">
         <input type="text" value={props.password} disabled />
-        <button onClick={() => navigator.clipboard.writeText(props.password)}>
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(props.password);
+            alert("Password Copied");
+          }}
+        >
           <FontAwesomeIcon
             icon={faCopy}
             style={{ fontSize: "20px", color: "white" }}
